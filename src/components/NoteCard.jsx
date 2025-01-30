41f1414c508656542569b6774490211870e38dbe
Ali Ramazan Yildirim <aliramazanyildirim@gmail.com>
2025-01-31 00:08:39 +0100

    Füge automatische Größenanpassung für Textarea in NoteCard-Komponente hinzu

--- src/components/NoteCard.jsx
+++ src/components/NoteCard.jsx
@@ -1,23 +1,54 @@
+import { useRef, useEffect } from "react";
 import Trash from "../icons/Trash";
 import useNotesStore from "../store/useNotesStore";
 
 const NoteCard = () => {
-    const notes = useNotesStore((state) => state.notes);
-    
-    return (
-        <>
-            {notes.map((note) => (
-                <div key={note.$id} className="card" style={{ backgroundColor: note.colors.colorBody, left: `${note.position.x}px`, top: `${note.position.y}px` }}>
-                    <div className="card-header" style={{ backgroundColor: note.colors.colorHeader }}>
-                        <Trash />
-                    </div>
-                    <div className="card-body">
-                        <textarea style={{ color: note.colors.colorText }} defaultValue={note.body}></textarea>
-                    </div>
-                </div>
-            ))}
-        </>
-    );
+  const notes = useNotesStore((state) => state.notes);
+  const textAreaRefs = useRef({});
+
+  useEffect(() => {
+    Object.values(textAreaRefs.current).forEach((ref) => ref && autoGrow(ref));
+  }, [notes]);
+
+  const autoGrow = (textAreaRef) => {
+    if (textAreaRef) {
+      textAreaRef.style.height = "auto"; // Höhe auf Auto zurücksetzen
+      textAreaRef.style.height = textAreaRef.scrollHeight + "px"; // Höhe auf ScrollHeight setzen
+    }
+  };
+
+  return (
+    <>
+      {notes.map((note) => (
+        <div
+          key={note.$id}
+          className="card"
+          style={{
+            backgroundColor: note.colors.colorBody,
+            left: `${note.position.x}px`,
+            top: `${note.position.y}px`,
+          }}
+        >
+          <div
+            className="card-header"
+            style={{ backgroundColor: note.colors.colorHeader }}
+          >
+            <Trash />
+          </div>
+          <div className="card-body">
+            <textarea
+              ref={(el) => (textAreaRefs.current[note.$id] = el)}
+              style={{ color: note.colors.colorText }}
+              defaultValue={note.body}
+              onInput={() => {
+                autoGrow(textAreaRefs.current[note.$id]);
+              }}
+            ></textarea>
+          </div>
+        </div>
+      ))}
+    </>
+  );
 };
 
 export default NoteCard;
